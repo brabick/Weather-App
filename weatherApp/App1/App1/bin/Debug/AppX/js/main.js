d4767aa2283d3bdc58a6a1b0a40bee5a54b8680c
Brandon <brabick@gmail.com>
2019-04-10 10:43:47 -0600

    Create weatherApp
    This is a web application that allows the user to drag the placement marker to anywhere on the map.  Then, the current weather data is displayed on the screen in a div.

---
+++ weatherApp/App1/App1/bin/Debug/AppX/js/main.js
@@ -0,0 +1,21 @@
+﻿// Your code here!
+// Your code here!
+
+var api="https://openweathermap.org/data/2.5/find?";
+var lon = "156";
+var lat = "185";
+var apiKey = "&APPID=d719c01cf81b957caff18cdb3a02ffe2";
+var units = "&units=metric";
+
+var weather;
+function setup() {
+    createCanvas(400, 200);
+    var url = api + city + apiKey + units;
+    loadJSON(url, gotData);
+}
+
+
+
+function gotData(data) {
+    weather = data;
+}
---
+++ weatherApp/weatherApp/bin/Debug/AppX/js/main.js
@@ -0,0 +1,40 @@
+﻿mapboxgl.accessToken = 'pk.eyJ1IjoiYnJhYmljayIsImEiOiJjanU5aHZweXIwNmJuNDRxcWpnMWphZGRtIn0.lcDz2CoUjLPZF12GPQT6Gw';
+var map = new mapboxgl.Map({
+    container: 'map', // container id
+    style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+    center: [-74.50, 40], // starting position [lng, lat]
+    zoom: 3 // starting zoom
+});
+
+var marker = new mapboxgl.Marker({
+    draggable: true
+})
+    .setLngLat([-78.96196465145502, 34.05093386644228])
+    .addTo(map);
+
+function onDragEnd() {
+
+    var lngLat = marker.getLngLat();
+    coordinates.style.display = 'block';
+    coordinates.innerHTML = 'Longitude: ' + lngLat.lng + '<br />Latitude: ' + lngLat.lat;
+
+    var api = "http://api.openweathermap.org/data/2.5/weather?";
+    var lon = "lon=";
+    var lat = "lat=";
+    var lonValue = lngLat.lng;
+    var latValue = lngLat.lat;
+    var apiKey = "&appid=d719c01cf81b957caff18cdb3a02ffe2";
+    var units = "&units=metric";
+    var fullUrl = api + lat + latValue + "&" + lon + lonValue + apiKey + units;
+    $.get(fullUrl, function (data, status) {
+        console.log(data.main.temp);
+        weatherData.innerHTML = "The temperature in " + data.name + " is " + data.main.temp + "C";
+    })
+
+
+}
+
+
+marker.on('dragend', onDragEnd);
+
+
